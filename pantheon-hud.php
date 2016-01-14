d599b90312a97a248611a28ee4d1f878a662279f
Daniel Bachhuber <daniel@handbuilt.co>
2016-01-14 04:43:44 -0800

    Initial commit of plugin scaffold

---
+++ pantheon-hud.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Pantheon HUD
+ * Version: 0.1-alpha
+ * Description: A heads-up display into your Pantheon environment.
+ * Author: Pantheon
+ * Author URI: https://pantheon.io
+ * Plugin URI: https://pantheon.io
+ * Text Domain: pantheon-hud
+ * Domain Path: /languages
+ * @package Pantheon HUD
+ */
