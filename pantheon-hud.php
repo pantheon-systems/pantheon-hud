58269bcbee7a063f32d2260a1402b1da804e89e6
Daniel Bachhuber <daniel@handbuilt.co>
2017-02-27 07:49:21 -0800

    Update readme for v0.1.1

--- pantheon-hud.php
+++ pantheon-hud.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Pantheon HUD
- * Version: 0.1.0
+ * Version: 0.1.1
  * Description: A heads-up display into your Pantheon environment.
  * Author: Pantheon
  * Author URI: https://pantheon.io
