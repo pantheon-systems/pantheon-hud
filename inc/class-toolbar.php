5305ce14f0257baebed59b1972daf9d5dfd58bf9
Daniel Bachhuber <daniel@handbuilt.co>
2020-02-24 14:12:22 -0800

    Rewrite using vanilla JavaScript

--- inc/class-toolbar.php
+++ inc/class-toolbar.php
@@ -158,13 +158,29 @@ class Toolbar {
 			admin_url( 'admin-ajax.php' )
 		);
 		$script      = <<<EOT
-jQuery('#wp-admin-bar-pantheon-hud').on('hover', function() {
-	if (jQuery('#wp-admin-bar-pantheon-hud-wp-admin-loading').length) {
-		jQuery.get('{$request_url}', function(data) {
-			jQuery('#wp-admin-bar-pantheon-hud .ab-sub-wrapper').html(data);
-		});
+(function(){
+	var el = document.querySelector('#wp-admin-bar-pantheon-hud');
+	if ( ! el ) {
+		return;
 	}
-});
+	var fetchData = function() {
+		if ( ! document.querySelector('#wp-admin-bar-pantheon-hud-wp-admin-loading') ) {
+			return;
+		}
+		var request = new XMLHttpRequest();
+		request.open('GET', '{$request_url}', true);
+		request.onload = function() {
+			if (this.status >= 200 && this.status < 400) {
+				document.querySelector('#wp-admin-bar-pantheon-hud .ab-sub-wrapper').innerHTML = this.response;
+				el.removeEventListener('mouseover', fetchData);
+				el.removeEventListener('focus', fetchData);
+			}
+		};
+		request.send();
+	};
+	el.addEventListener('mouseover', fetchData);
+	el.addEventListener('focus', fetchData);
+}());
 EOT;
 		wp_add_inline_script( 'admin-bar', $script );
 add_filter(
