73443456db463752ec4959652756ded9109ef969
Chris Reynolds <chris@jazzsequence.com>
2023-07-05 11:51:35 -0600

    exit for errors

--- bin/phpunit-test.sh
+++ bin/phpunit-test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-bash ./bin/install-wp-tests.sh wordpress_test root root 127.0.0.1 latest
+
+set -e
 echo "Running PHPUnit on Single Site"
 composer phpunit
 rm -rf $WP_TESTS_DIR $WP_CORE_DIR
