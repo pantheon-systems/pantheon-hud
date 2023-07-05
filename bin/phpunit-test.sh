c5721486eab8bf99c15cf596261f360b1689bdca
Chris Reynolds <chris@jazzsequence.com>
2023-07-05 15:39:32 -0600

    use realpath

--- bin/phpunit-test.sh
+++ bin/phpunit-test.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-DIRNAME=$(dirname = "$0")
+DIRNAME=$(realpath "$(dirname = "$0")")
 
 bash "${DIRNAME}/install-wp-tests.sh" wordpress_test root root 127.0.0.1 latest
 echo "Running PHPUnit on Single Site"
