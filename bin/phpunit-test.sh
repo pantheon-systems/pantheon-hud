0a0d1ef3c29efb7f8e82903247a324de60f37375
Chris Reynolds <chris@jazzsequence.com>
2023-07-05 15:42:51 -0600

    update dirname

--- bin/phpunit-test.sh
+++ bin/phpunit-test.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-DIRNAME=$(realpath "$(dirname = "$0")")
+DIRNAME=$(cd "$(dirname = "$0")" && pwd)
 
 bash "${DIRNAME}/install-wp-tests.sh" wordpress_test root root 127.0.0.1 latest
 echo "Running PHPUnit on Single Site"
