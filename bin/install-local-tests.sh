c62f21df7382dd841c9e692951b0fce3feebd4b8
Chris Reynolds <chris@jazzsequence.com>
2023-10-04 11:50:10 -0600

    allow any install-wp-tests parameter to be passed in

--- bin/install-local-tests.sh
+++ bin/install-local-tests.sh
@@ -1,20 +1,60 @@
 #!/bin/bash
 set -e
 
-# Set TMPDIR to /tmp.
+# Initialize variables with default values
 TMPDIR="/tmp"
-
-# Initialize a variable to hold the 'true' flag for skipping DB creation
+DB_NAME="wordpress_test"
+DB_USER="root"
+DB_PASS=""
+DB_HOST="127.0.0.1"
+WP_VERSION="latest"
 SKIP_DB=""
 
-# Check if '--no-db' is passed as an argument
-if [[ "$1" == "--no-db" ]]; then
-  SKIP_DB="true"
-fi
+# Display usage information
+usage() {
+  echo "Usage:"
+  echo "./install-local-tests.sh [--dbname=wordpress_test] [--dbuser=root] [--dbpass=''] [--dbhost=127.0.0.1] [--wpversion=latest] [--no-db]"
+}
+
+# Parse command-line arguments
+for i in "$@"
+do
+case $i in
+    --dbname=*)
+    DB_NAME="${i#*=}"
+    shift
+    ;;
+    --dbuser=*)
+    DB_USER="${i#*=}"
+    shift
+    ;;
+    --dbpass=*)
+    DB_PASS="${i#*=}"
+    shift
+    ;;
+    --dbhost=*)
+    DB_HOST="${i#*=}"
+    shift
+    ;;
+    --wpversion=*)
+    WP_VERSION="${i#*=}"
+    shift
+    ;;
+    --no-db)
+    SKIP_DB="true"
+    shift
+    ;;
+    *)
+    # unknown option
+    usage
+    exit 1
+    ;;
+esac
+done
 
 # Run install-wp-tests.sh
 echo "Installing local tests into ${TMPDIR}"
-bash "$(dirname "$0")/install-wp-tests.sh" wordpress_test root '' 127.0.0.1 latest $SKIP_DB
+bash "$(dirname "$0")/install-wp-tests.sh" "$DB_NAME" "$DB_USER" "$DB_PASS" "$DB_HOST" "$WP_VERSION" "$SKIP_DB"
 
 # Run PHPUnit
 echo "Running PHPUnit"
