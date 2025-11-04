71ff21b51b49657ab78f23e3d5bfb3c996ff96d8
Andrii KOSTENETSKYI <andrii.kostenetsky@pantheon.io>
2025-11-04 13:15:11 +0200

    feat: SRE-1388 - Migration from CircleCI to GH Actions

--- bin/behat-prepare.sh
+++ bin/behat-prepare.sh
@@ -8,18 +8,18 @@
 
 terminus whoami > /dev/null
 if [ $? -ne 0 ]; then
-  echo "Terminus unauthenticated; assuming unauthenticated build"
-  exit 0
+	echo "Terminus unauthenticated; assuming unauthenticated build"
+	exit 0
 fi
 
 if [ -z "$TERMINUS_SITE" ] || [ -z "$TERMINUS_ENV" ]; then
-  echo "TERMINUS_SITE and TERMINUS_ENV environment variables must be set"
-  exit 1
+	echo "TERMINUS_SITE and TERMINUS_ENV environment variables must be set"
+	exit 1
 fi
 
 if [ -z "$WORDPRESS_ADMIN_USERNAME" ] || [ -z "$WORDPRESS_ADMIN_PASSWORD" ]; then
-  echo "WORDPRESS_ADMIN_USERNAME and WORDPRESS_ADMIN_PASSWORD environment variables must be set"
-  exit 1
+	echo "WORDPRESS_ADMIN_USERNAME and WORDPRESS_ADMIN_PASSWORD environment variables must be set"
+	exit 1
 fi
 
 set -ex
