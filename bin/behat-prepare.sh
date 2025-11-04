b1762cbb9f0c76bc5586fc9be5b3bed071342163
Andrii KOSTENETSKYI <andrii.kostenetsky@pantheon.io>
2025-11-04 13:14:13 +0200

    feat: SRE-1388 - Migration from CircleCI to GH Actions

--- bin/behat-prepare.sh
+++ bin/behat-prepare.sh
@@ -25,12 +25,6 @@ fi
 set -ex
 
 ###
-# Install Composer dependencies, including Behat. This makes the
-# ./vendor/bin/behat executable available for the test runner.
-###
-composer install --no-progress --prefer-dist
-
-###
 # Check for and apply any outstanding upstream updates.
 # This never happens manually, so we might as well do it in automation before we run tests.
 ###
